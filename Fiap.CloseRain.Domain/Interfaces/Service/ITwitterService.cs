f206a9f9992f52d8156fcd30383e6d83e41e04c5
Eduardo Rodrigues Veras <eduardo.veras@xpi.com.br>
2019-08-05 17:44:05 -0300

    Serviços externos

--- Fiap.CloseRain.Domain/Interfaces/Service/ITwitterService.cs
+++ Fiap.CloseRain.Domain/Interfaces/Service/ITwitterService.cs
@@ -1,7 +1,11 @@
-﻿namespace Fiap.CloseRain.Domain.Interfaces.Service
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace Fiap.CloseRain.Domain.Interfaces.Service
 {
     public interface ITwitterService
-    {
-        
+    {   
+        Task<bool> Tweet(string message);
+        Task<bool> TweetWithCoordinates(double longitude, double latitude, string message);
     }
 }
---
+++ Fiap.CloseRain.Service.Twitter/Services/TwitterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Fiap.CloseRain.Service.Twitter.Services
+{
+    public class TwitterService
+    {
+        public void Lembrar()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+    }
+}
